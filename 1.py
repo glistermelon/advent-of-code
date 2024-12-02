8f10a665be848b35d9125d89015a548cfc5a9c92
glistermelon <noah.railsbacka@gmail.com>
2024-12-02 09:45:58 -0600

    Day 1

---
+++ 1.py
@@ -0,0 +1,10 @@
+left = []
+right = []
+for ln in open('inputs/1.txt'):
+    ln = ln.split()
+    left.append(int(ln[0]))
+    right.append(int(ln[1]))
+left.sort()
+right.sort()
+print(sum(abs(right[i] - left[i]) for i in range(len(left))))
+print(sum(x * right.count(x) for x in left))
