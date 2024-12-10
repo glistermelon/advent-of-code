248db85de7c941fcb48360e2346861c9be225b0f
Glistermelon <th3greatm3lon@gmail.com>
2024-12-09 23:38:23 -0600

    Cleaner day 10 python

--- python/10.py
+++ python/10.py
@@ -4,27 +4,25 @@ board = [[int(c) for c in ln] for ln in open('../inputs/10.txt').read().splitlin
 
 part2 = False
 
-def search(done : list[(int, int)] = []):
-    y, x = done[-1]
+def search(nodes : list[(int, int)] = []):
+    y, x = nodes[-1]
     if board[y][x] == 9:
         return 1 if part2 else set(((y, x),))
     ret = 0 if part2 else set()
     for dx in (-1, 0, 1):
         for dy in (-1, 0, 1):
-            if dx != 0 and dy != 0:
-                continue
-            p1 = (y + dy, x + dx)
-            if p1[0] < 0 or p1[1] < 0 or p1[0] >= len(board) or p1[1] >= len(board[0]):
-                continue
-            if p1 in done:
-                continue
-            if board[p1[0]][p1[1]] - board[y][x] == 1 and p1 not in done:
-                done.append(p1)
+            if dx != 0 and dy != 0: continue
+            n1 = (y + dy, x + dx)
+            if n1[0] < 0 or n1[1] < 0 or n1[0] >= len(board) or n1[1] >= len(board[0]): continue
+            if board[n1[0]][n1[1]] - board[y][x] != 1: continue
+            if n1 in nodes: continue
+            if board[n1[0]][n1[1]] - board[y][x] == 1:
+                nodes.append(n1)
                 if part2:
-                    ret += search(done)
+                    ret += search(nodes)
                 else:
-                    ret.update(search(done))
-                done.pop()
+                    ret.update(search(nodes))
+                nodes.pop()
     return ret
 
 print(sum(len(search([(y, x)])) for y, x in find_nd(board, 0)))
