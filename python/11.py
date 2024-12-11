10c5981a1c7ac4b0c4c3f9b55d484c0ce567dd05
Glistermelon <th3greatm3lon@gmail.com>
2024-12-10 23:49:14 -0600

    Day 11

---
+++ python/11.py
@@ -0,0 +1,30 @@
+from util import *
+
+stones = [int(s) for s in open('../inputs/11.txt').read().split()]
+stones = { s : stones.count(s) for s in stones }
+
+def add_to_map(m, k, v):
+    try:
+        m[k] += v
+    except:
+        m[k] = v
+
+def blink(stones):
+    new = {}
+    for s, c in stones.items():
+        if s == 0:
+            add_to_map(new, 1, c)
+            continue
+        ss = str(s)
+        if len(ss) % 2 == 0:
+            add_to_map(new, int(ss[len(ss) // 2:]), c)
+            add_to_map(new, int(ss[:len(ss) // 2]), c)
+        else:
+            add_to_map(new, s * 2024, c)
+    return new
+
+for i in range(75):
+    if i == 25:
+        print(sum(stones.values()))
+    stones = blink(stones)
+print(sum(stones.values()))
