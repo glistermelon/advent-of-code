ab9974312526c9a0d495bf09ed808ec6bf2677d4
Glistermelon <th3greatm3lon@gmail.com>
2025-04-06 19:38:45 -0500

    Template Day class

---
+++ 2023/src/main/java/glistermelon/Day0.java
@@ -0,0 +1,21 @@
+package glistermelon;
+
+import java.io.IOException;
+
+// copyable template
+
+public class Day0 extends DaySolver {
+
+    public Day0() throws IOException {
+        super(0);
+    }
+
+    public String solvePart1() {
+        return "";
+    }
+
+    public String solvePart2() {
+        return "";
+    }
+
+}
