9b1c54fd7651fe1133f16439d6cee2d289367933
glistermelon <glisterm3lon@gmail.com>
2025-04-07 20:50:44 -0500

    2023 Day 8 Part 2

--- 2023/src/main/java/glistermelon/Day8.java
+++ 2023/src/main/java/glistermelon/Day8.java
@@ -2,7 +2,9 @@ package glistermelon;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day8 extends DaySolver {
 
@@ -39,7 +41,65 @@ public class Day8 extends DaySolver {
     }
 
     public String solvePart2() {
-        return "";
+
+        String[] lines = getPuzzleInputLines();
+        String pattern = lines[0];
+        List<Node> nodes = new ArrayList<>();
+        for (int i = 2; i < lines.length; i++) {
+            String line = lines[i];
+            Node node = new Node(
+                    line.substring(0, 3),
+                    line.substring(7, 10),
+                    line.substring(12, 15)
+            );
+            if (node.name.endsWith("A")) nodes.add(node);
+        }
+
+        long[] starts = new long[nodes.size()];
+        long[] periods = new long[starts.length];
+
+        for (int nodeIndex = 0; nodeIndex < nodes.size(); nodeIndex++) {
+
+            Node node = nodes.get(nodeIndex);
+            int patternIndex = 0;
+            long steps = 0;
+            while (!node.name.endsWith("Z")) {
+                if (pattern.charAt(patternIndex) == 'R') node = node.rightNode;
+                else node = node.leftNode;
+                if (++patternIndex == pattern.length()) patternIndex = 0;
+                steps++;
+            }
+            starts[nodeIndex] = steps;
+
+            List<Integer> subperiods = new ArrayList<>();
+            for (int repeats = 0; true; repeats++) {
+                for (int i = 0; i < pattern.length(); i++) {
+
+                    if (pattern.charAt(patternIndex) == 'R') node = node.rightNode;
+                    else node = node.leftNode;
+                    if (++patternIndex == pattern.length()) patternIndex = 0;
+
+                    int p = pattern.length() * repeats + i + 1;
+                    if (node.name.endsWith("Z")) subperiods.add(p);
+                    else subperiods.removeAll(subperiods.stream().filter(sp -> p % sp == 0).toList());
+
+                }
+                if (node.name.endsWith("Z")) break;
+            }
+            periods[nodeIndex] = subperiods.stream().min(Integer::compareTo).get();
+
+        }
+
+        long minStart = Arrays.stream(starts).min().getAsLong();
+        long period = 0;
+        for (int i = 0; i < starts.length; i++) {
+            if (starts[i] == minStart) period = periods[i];
+            starts[i] -= minStart;
+        }
+        long steps = 0;
+
+        return String.valueOf(steps);
+
     }
 
     class Node {
