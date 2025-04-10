d0c6b61eb57ec10b2c7a5d0249d17fad01219f34
glistermelon <glisterm3lon@gmail.com>
2025-04-10 01:15:51 +0100

    Fix doubleToString again

--- 2023/src/main/java/glistermelon/Util.java
+++ 2023/src/main/java/glistermelon/Util.java
@@ -4,7 +4,7 @@ public class Util {
 
     static String doubleToString(double n, int decimalDigits) {
         String s = String.valueOf(n);
-        if (s.chars().anyMatch(c -> !Character.isDigit(c))) return s;
+        if (s.chars().anyMatch(c -> c != '.' && !Character.isDigit(c))) return s;
         return s.substring(0, Integer.min(s.indexOf('.') + decimalDigits + 1, s.length()));
     }
 
