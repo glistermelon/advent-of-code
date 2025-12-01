83c46148500246501d004381ff1bbc40be88ac2e
glistermelon <glisterm3lon@gmail.com>
2025-12-01 01:39:08 -0500

    Actually include both parts for 2025 Day 1

--- 2025/python/1.py
+++ 2025/python/1.py
@@ -1,19 +1,40 @@
 puzzle_input = open('../inputs/1.txt').read()
 
-pos = 50
-pw = 0
-for instr in puzzle_input.splitlines(keepends=False):
-    sign = 1 if instr[0] == 'R' else -1
-    delta = int(instr[1:])
-    pos_og_zero = pos == 0
-    pos += sign * delta
-    pw_delta, pos = divmod(pos, 100)
-    pw_delta = abs(pw_delta)
-    if sign == -1:
-        if pos_og_zero:
-            pw_delta -= 1
-        if pos == 0:
-            pw_delta += 1
-    pw += pw_delta
+instructions = None
 
-print(pw)
+def process_input():
+    global instructions
+    instructions = []
+    for ln in puzzle_input.splitlines(keepends=False):
+        sign = 1 if ln[0] == 'R' else -1
+        delta = int(ln[1:])
+        instructions.append((sign, delta))
+
+def solve_part_1():
+    pos = 50
+    pw = 0
+    for sign, delta in instructions:
+        pos = (pos + sign * delta) % 100
+        if pos == 0:
+            pw += 1
+    return pw
+
+def solve_part_2():
+    pos = 50
+    pw = 0
+    for sign, delta in instructions:
+        pos_og_zero = pos == 0
+        pos += sign * delta
+        pw_delta, pos = divmod(pos, 100)
+        pw_delta = abs(pw_delta)
+        if sign == -1:
+            if pos_og_zero:
+                pw_delta -= 1
+            if pos == 0:
+                pw_delta += 1
+        pw += pw_delta
+    return pw
+
+process_input()
+print(solve_part_1())
+print(solve_part_2())
