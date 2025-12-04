d9b7cadc29b324ae8c999eec51e783e9eae99bb0
glistermelon <glisterm3lon@gmail.com>
2025-12-04 10:36:04 -0500

    Crudely ported 2018 days 1 & 2 to new framework

--- 2018/cpp/1.cpp
+++ 2018/cpp/1.cpp
@@ -1,44 +1,46 @@
-#include "include.hpp"
+#include "DaySolver.hpp"
 
-std::string solve_part_1(std::string puzzle_input) {
+class Day1Solver : public DaySolver {
 
-    std::istringstream puzzle_input_istream(puzzle_input);
-    std::string line;
-    int sum = 0;
-    while (std::getline(puzzle_input_istream, line, '\n')) {
-        sum += std::stoi(line);
-    }
-    return std::to_string(sum);
-    
-}
+protected:
 
-std::string solve_part_2(std::string puzzle_input) {
+    DaySolution solve_part_1() {
 
-    std::istringstream puzzle_input_istream(puzzle_input);
-    std::string line;
-    std::vector<int> deltas;
-    while (std::getline(puzzle_input_istream, line, '\n')) {
-        deltas.push_back(std::stoi(line));
-    }
-
-    int sum = 0;
-    std::set<int> history = { sum };
-    while (true) {
-        for (int delta : deltas) {
-            sum += delta;
-            if (history.count(sum)) {
-                return std::to_string(sum);
-            }
-            history.insert(sum);
+        std::istringstream puzzle_input_istream(puzzle_input);
+        std::string line;
+        int sum = 0;
+        while (std::getline(puzzle_input_istream, line, '\n')) {
+            sum += std::stoi(line);
         }
+        return std::to_string(sum);
+        
     }
 
-}
+    DaySolution solve_part_2() {
 
-int main() {
-    
-    std::string puzzle_input = get_puzzle_input(1);
-    std::cout << solve_part_1(puzzle_input) << std::endl;
-    std::cout << solve_part_2(puzzle_input) << std::endl;
-    
+        std::istringstream puzzle_input_istream(puzzle_input);
+        std::string line;
+        std::vector<int> deltas;
+        while (std::getline(puzzle_input_istream, line, '\n')) {
+            deltas.push_back(std::stoi(line));
+        }
+
+        int sum = 0;
+        std::set<int> history = { sum };
+        while (true) {
+            for (int delta : deltas) {
+                sum += delta;
+                if (history.count(sum)) {
+                    return std::to_string(sum);
+                }
+                history.insert(sum);
+            }
+        }
+
+    }
+
+};
+
+DaySolver* DaySolver::get_current_day_solver() {
+    return new Day1Solver();
 }
